3df3362c1bdfbdac488c7a3e5724a9d84de44b8c
Fazlul Kabir <fazlulkabir34@gmail.com>
2019-09-14 21:43:05 +0600

    Update EventsController.php

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -11,29 +11,29 @@ use App\Event;
 class EventsController extends Controller
 {
     //
-    public function index()
-    {
-        $events = [];
-        $data = Event::all();
-        if($data->count()) {
-            foreach ($data as $key => $value) {
-                $events[] = Calendar::event(
-                    $value->title,
-                    true,
-                    new \DateTime($value->start_date),
-                    new \DateTime($value->end_date.' +1 day'),
-                    null,
-                    // Add color and link on event
-	                [
-	                    'color' => '#f05050',
-	                    'url' => 'pass here url and any route',
-	                ]
-                );
-            }
-        }
-        $calendar = Calendar::addEvents($events);
-        return view('fullcalender')->with('calendar',$calendar);
-    }
+    // public function index()
+    // {
+    //     $events = [];
+    //     $data = Event::all();
+    //     if($data->count()) {
+    //         foreach ($data as $key => $value) {
+    //             $events[] = Calendar::event(
+    //                 $value->title,
+    //                 true,
+    //                 new \DateTime($value->start_date),
+    //                 new \DateTime($value->end_date.' +1 day'),
+    //                 null,
+    //                 // Add color and link on event
+	   //              [
+	   //                  'color' => '#f05050',
+	   //                  'url' => 'pass here url and any route',
+	   //              ]
+    //             );
+    //         }
+    //     }
+    //     $calendar = Calendar::addEvents($events);
+    //     return view('fullcalender')->with('calendar',$calendar);
+    // }
     public function store(Request $request)
     {
         // $event= new Event();
